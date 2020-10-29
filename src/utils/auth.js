1702894d07dbe74d52e63eff4df7e57bff732761
sjiaw <sjiaw1018@gmail.com>
2020-10-29 10:00:03 +0800

    localstorage存储 代替 cookies存储，以便在 electron 后正常使用

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -4,25 +4,31 @@ const TokenKey = 'vue_token'
 const Uid = 'vue_uid'
 
 export function getToken() {
-  return Cookies.get(TokenKey)
+  // return Cookies.get(TokenKey)
+  return localStorage.getItem(TokenKey)
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+  // return Cookies.set(TokenKey, token)
+  return localStorage.setItem(TokenKey, token)
 }
 
 export function removeToken() {
-  return Cookies.remove(TokenKey)
+  // return Cookies.remove(TokenKey)
+  return localStorage.removeItem(TokenKey)
 }
 
 export function getUid() {
-  return Cookies.get(Uid)
+  // return Cookies.get(Uid)
+  return localStorage.getItem(Uid)
 }
 
 export function setUid(uid) {
-  return Cookies.set(Uid, uid)
+  // return Cookies.set(Uid, uid)
+  return localStorage.setItem(Uid, uid)
 }
 
 export function removeUid() {
-  return Cookies.remove(Uid)
+  // return Cookies.remove(Uid)
+  return localStorage.removeItem(Uid)
 }
